490d94873f89d431c6f2be05d107ec9bbba83f9f
Bryan <bryanelliw@gmail.com>
2021-07-01 19:21:59 -0300

    refazendo css do site cifra de césar

--- cifra-de-cesar/javascript/script.js
+++ cifra-de-cesar/javascript/script.js
@@ -7,20 +7,20 @@ const cesar = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
             'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C']
 
 function crypt(){
-    let mensagem = document.getElementById("mensagem");
-    let msg = String(mensagem.value);
-    let msgdiv = document.getElementById("divmsg");
-    let crypto = msg.split("");
-    let frase = "";
-    for (let cont = 0; crypto.length > cont; cont++){
-        for(let aux = 0; cesar.length > aux; aux++){
+    var mensagem = document.getElementById("mensagem");
+    var msg = String(mensagem.value);
+    var msgdiv = document.getElementById("divmsg");
+    var crypto = msg.split("");
+    var frase = "";
+    for (var cont = 0; crypto.length > cont; cont++){
+        for(var aux = 0; cesar.length > aux; aux++){
             if(crypto[cont].toUpperCase() === alpha[aux]){
                 crypto[cont] = cesar[aux];
                 break;
             }
         }
     }
-    for(let contador = 0; crypto.length > contador; contador++){
+    for(var contador = 0; crypto.length > contador; contador++){
         if(contador == 0){
             frase += crypto[contador].toUpperCase();
         }
@@ -32,20 +32,20 @@ function crypt(){
 }
 
 function uncrypt(){
-    let mensagem = document.getElementById("mensagem");
-    let msg = String(mensagem.value);
-    let uncrypt = msg.split("");
-    let msgdiv = document.getElementById("divmsg");
-    let frase = "";
-    for(let cont = 0; uncrypt.length > cont; cont++){
-        for(let aux = 0; alpha.length > aux; aux++){
+    var mensagem = document.getElementById("mensagem");
+    var msg = String(mensagem.value);
+    var uncrypt = msg.split("");
+    var msgdiv = document.getElementById("divmsg");
+    var frase = "";
+    for(var cont = 0; uncrypt.length > cont; cont++){
+        for(var aux = 0; alpha.length > aux; aux++){
             if(uncrypt[cont].toUpperCase() === cesar[aux]){
                 uncrypt[cont] = alpha[aux];
                 break;
             }
         }
     }
-    for(let contador = 0; uncrypt.length > contador; contador++){
+    for(var contador = 0; uncrypt.length > contador; contador++){
         if(contador == 0){
             frase += uncrypt[contador].toUpperCase();
         } else {
